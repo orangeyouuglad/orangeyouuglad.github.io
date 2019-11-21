6f127e79dd5d1a185b811a85abc19ec4fd90913d
orangeyouuglad <56795603+orangeyouuglad@users.noreply.github.com>
2019-11-20 23:18:10 -0500

    Delete app.js

--- Grassroots/app.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.bannerButton').addEventListener('click', () => {
-    document.querySelector('.landing').style.display = 'none';
-    document.querySelector('.form').style.cssText = 'opacity: 1; visibility: visible';
-});
-
-document.querySelector('.xButton').addEventListener('click', () => {
-    document.querySelector('.landing').style.display = 'flex';
-    document.querySelector('.form').style.cssText = 'opacity: 0; visibility: hidden';
-})
